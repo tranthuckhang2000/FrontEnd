7b17b30065ffb517e9330b6660c473bdb0fcbc9b
huuthang1107 <18130214@st.hcmuaf.edu.vn>
2021-06-08 15:30:43 +0700

    update header footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
-  template: '<app-home></app-home>',
+  template: '<app-header></app-header>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
