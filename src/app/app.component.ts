c18ddcdab07fe178039974edda093c17366bc044
dangkiet2000 <dangvankiet.11c5@gmail.com>
2021-06-22 07:40:42 +0700

    fix home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
-  template: '<app-trongnuoc_menu></app-trongnuoc_menu> <app-quocte_menu></app-quocte_menu>' +
-    '<app-congdoan-menu></app-congdoan-menu> <app-bandoc-menu></app-bandoc-menu> ' +
-    '<app-kinhte-menu></app-kinhte-menu> <app-suckhoe-menu></app-suckhoe-menu> <app-giaoduc-menu></app-giaoduc-menu>',
+  template: '<app-trongnuoc_menu></app-trongnuoc_menu>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
