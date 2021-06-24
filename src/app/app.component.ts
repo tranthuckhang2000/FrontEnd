530b8c237153c5866ed4b03b94881e3177186ad9
dangkiet2000 <dangvankiet.11c5@gmail.com>
2021-06-24 16:54:09 +0700

    update category_menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   // templateUrl: './app.component.html',
   template: '<app-trongnuoc_menu></app-trongnuoc_menu> <app-quocte_menu></app-quocte_menu>' +
-    '<app-congdoan-menu></app-congdoan-menu> <app-bandoc-menu></app-bandoc-menu> ' +
-    '<app-kinhte-menu></app-kinhte-menu> <app-suckhoe-menu></app-suckhoe-menu> <app-giaoduc-menu></app-giaoduc-menu>',
+    '<app-bandoc-menu></app-bandoc-menu> <app-giaoduc-menu></app-giaoduc-menu> <app-kinhte-menu></app-kinhte-menu>' +
+    '<app-suckhoe-menu></app-suckhoe-menu> <app-congdoan-menu></app-congdoan-menu>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
