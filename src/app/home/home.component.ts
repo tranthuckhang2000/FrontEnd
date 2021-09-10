de228b8787371948e72175626f90cdb8f16cec03
huuthang1107 <18130214@st.hcmuaf.edu.vn>
2021-09-10 13:52:56 +0700

    Dỗ dữ liệu chỉnh sửa trang home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FeedService} from "../feed/service/feed.service";
+import {FeedItems} from "../feed/model/feed-items";
 
 @Component({
   selector: 'app-home',
@@ -6,22 +8,109 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  imgLink: string;
-  title: string;
-  content: string;
-  tinnong: string = 'Tới lượt Mỹ bị điều tra về nguồn gốc dịch Covid-19?';
-  listItem: any = [1,2,3,4,5,6];
-  listItem2: any = [1,2,3,4];
-  listItem3: any = [1,2,3];
 
-  image: string = './assets/images/big_trump.jpg';
-  constructor() { }
+  // @ts-ignore
+  feed = new FeedService();
+  data: FeedItems[];
+  data2: FeedItems[];
+  data3: FeedItems[];
+  data4:FeedItems[];
+  data5:FeedItems[];
+  data6:FeedItems[];
+  data7:FeedItems[];
+  data8:FeedItems[];
+  data9:FeedItems[];
+  data10:FeedItems[];
+  data11:FeedItems[];
+  data12:FeedItems[];
+  data13:FeedItems[];
+  data14:FeedItems[];
+  data15:FeedItems[];
+  data16:FeedItems[];
+  TitleArrTNB: any;
+  ImgArrTNB: any;
+  title: string;
+  arrnew: any = [1,2,3,4,5,6,7,8,9,10];
+  titleXN: string = 'Khánh Hòa: Xuất hiện ca dương tính với SARS-CoV-2';
+  constructor(feedService: FeedService) {
+    this.data2 = [];
+    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Ftin-moi-nhat.rss")
+      .subscribe(value => {
+        feedService.createListFeedItems(this.data2,value)
+      })
+    this.data3 = [];
+    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fthoi-su.rss")
+      .subscribe(value => {
+        feedService.createListFeedItems(this.data3,value);
+      });
+    this.data4 = [];
+    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fthoi-su-quoc-te.rss")
+      .subscribe(value => {
+        feedService.createListFeedItems(this.data4,value);
+      });
+    this.data5 = [];
+    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fcong-doan.rss")
+      .subscribe(value => {
+        feedService.createListFeedItems(this.data5,value);
+      });
+    this.data6 = [];
+    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fban-doc.rss")
+      .subscribe(value => {
+        feedService.createListFeedItems(this.data6,value);
+      });
+    this.data7 = [];
+    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fkinh-te.rss")
+      .subscribe(value => {
+        feedService.createListFeedItems(this.data7,value);
+      });
+    this.data8 = [];
+    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fsuc-khoe.rss")
+      .subscribe(value => {
+        feedService.createListFeedItems(this.data8,value);
+      });
+    this.data9 = [];
+    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fgiao-duc-khoa-hoc.rss")
+      .subscribe(value => {
+        feedService.createListFeedItems(this.data9,value);
+      });
+    this.data10 = [];
+    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fphap-luat.rss")
+      .subscribe(value => {
+        feedService.createListFeedItems(this.data10,value);
+      });
+    this.data11 = [];
+    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fgiai-tri.rss")
+      .subscribe(value => {
+        feedService.createListFeedItems(this.data11,value);
+      });
+    this.data12 = [];
+    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fthe-thao.rss")
+      .subscribe(value => {
+        feedService.createListFeedItems(this.data12,value);
+      });
+    this.data13 = [];
+    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fcong-nghe.rss")
+      .subscribe(value => {
+        feedService.createListFeedItems(this.data13,value);
+      });
+    this.data14 = [];
+    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fdiem-den-hap-dan.rss")
+      .subscribe(value => {
+        feedService.createListFeedItems(this.data14,value);
+      });
+    this.data15 = [];
+    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fchuyen-trang-phu-nu.rss")
+      .subscribe(value => {
+        feedService.createListFeedItems(this.data15,value);
+      });
+    this.data16 = [];
+    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fthi-truong.rss")
+      .subscribe(value => {
+        feedService.createListFeedItems(this.data16,value);
+      });
+  }
 
   ngOnInit(): void {
-    this.imgLink = 'https://nld.mediacdn.vn/zoom/544_340/291774122806476800/2021/6/17/benh1a-1623886031186410911854.jpg';
-    this.title = 'Sáng 17-6, thêm 158 ca mắc Covid-19 trong nước, TP HCM có 45 ca';
-    this.content = '(NLĐO) - Sáng nay 17-6 ghi nhận thêm 159 ca Covid-19, gồm 1 ca nhập cảnh và 158 ca trong nước. Ngoài các điểm nóng Bắc Giang 87 ca, TP HCM 45 ca, ' +
-      'Tiền Giang và Bình Dương cũng phát hiện thêm nhiều ca bệnh với lần lượt 11 và 7 ca.';
 
   }
 
