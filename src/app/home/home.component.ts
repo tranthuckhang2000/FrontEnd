d591629ce8893d239fd7ebbe502c81a0a321a646
tranthuckhang2000 <73299797+tranthuckhang2000@users.noreply.github.com>
2021-09-20 10:42:58 +0700

    home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -43,69 +43,68 @@ export class HomeComponent implements OnInit {
   rssFeed: any[] = [];
 
   constructor(private feedService: FeedService, private http: HttpClient) {
-    this.test = [
-      {title: "trong nước", link: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fthoi-su.rss"},
-      {title: "công đoàn", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fcong-doan.rss "},
-      {title: "quốc tế", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fthoi-su-quoc-te.rss "},
-      {title: "bạn đọc", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fban-doc.rss "},
-      {title: "kinh tế", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fkinh-te.rss "},
-      {title: "sức khỏe", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fsuc-khoe.rss "},
-      {title: "giáo dục", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fgiao-duc.rss "},
-      {title: "pháp luật", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fphap-luat.rss "},
-      {title: "giải trí", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fgiai-tri.rss "},
-      {title: "thể thao", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fthe-thao.rss "},
-      {title: "công nghệ", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fcong-nghe.rss "},
-      {title: "phụ nữ", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fphu-nu.rss "},
-      {title: "địa ốc", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fdia-oc.rss "},
-      {title: "điểm đến", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fdiem-den.rss "},,
-    ]
-    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fban-doc.rss")
-    .subscribe(value => {
-      feedService.createListFeedItems(this.newsFeed, value);
-    });
-    feedService.getDataRss(" https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fthoi-su-quoc-te.rss")
-    .subscribe(value => {
-      feedService.createListFeedItems(this.nationalFeed,value);
-    });
-    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fcong-doan.rss")
-    .subscribe(value => {
-      feedService.createListFeedItems(this.unionFeed,value);
-    });
-    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fcong-nghe.rss")
-    .subscribe(value => {
-      feedService.createListFeedItems(this.techniqueFeed, value);
-    });
-    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fdia-oc.rss")
-    .subscribe(value => {
-      feedService.createListFeedItems(this.estateFeed, value);
-    });
-    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fdiem-den.rss")
-    .subscribe(value => {
-      feedService.createListFeedItems(this.destinationFeed, value);
-    });
-    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fgiai-tri.rss")
-    .subscribe(value => {
-      feedService.createListFeedItems(this.relaxFeed, value);
-    });
-    feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fgiao-duc.rss")
-    .subscribe(value => {
-      // console.log(value.items[0].link);
-      this.link = value.items[0].link;
-      // console.log(this.link)
-      this.onSend(this.link);
-      // console.log('link: ' + this.link);
-      this.feedService.onSendService(this.formData).subscribe(
-        res =>{
-          this.cover = res.content[0]
-          // console.log('cover: ' + this.cover);
-        },
-        error => {
-          console.log(error);
-        }
-      )
-      feedService.createListFeedItems(this.eduFeed, value);
-    });
-
+    // this.test = [
+    //   {title: "trong nước", link: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fthoi-su.rss"},
+    //   {title: "công đoàn", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fcong-doan.rss "},
+    //   {title: "quốc tế", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fthoi-su-quoc-te.rss "},
+    //   {title: "bạn đọc", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fban-doc.rss "},
+    //   {title: "kinh tế", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fkinh-te.rss "},
+    //   {title: "sức khỏe", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fsuc-khoe.rss "},
+    //   {title: "giáo dục", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fgiao-duc.rss "},
+    //   {title: "pháp luật", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fphap-luat.rss "},
+    //   {title: "giải trí", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fgiai-tri.rss "},
+    //   {title: "thể thao", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fthe-thao.rss "},
+    //   {title: "công nghệ", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fcong-nghe.rss "},
+    //   {title: "phụ nữ", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fphu-nu.rss "},
+    //   {title: "địa ốc", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fdia-oc.rss "},
+    //   {title: "điểm đến", link: " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fdiem-den.rss "},,
+    // ]
+    // feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fban-doc.rss")
+    // .subscribe(value => {
+    //   feedService.createListFeedItems(this.newsFeed, value);
+    // });
+    // feedService.getDataRss(" https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fthoi-su-quoc-te.rss")
+    // .subscribe(value => {
+    //   feedService.createListFeedItems(this.nationalFeed,value);
+    // });
+    // feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fcong-doan.rss")
+    // .subscribe(value => {
+    //   feedService.createListFeedItems(this.unionFeed,value);
+    // });
+    // feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fcong-nghe.rss")
+    // .subscribe(value => {
+    //   feedService.createListFeedItems(this.techniqueFeed, value);
+    // });
+    // feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fdia-oc.rss")
+    // .subscribe(value => {
+    //   feedService.createListFeedItems(this.estateFeed, value);
+    // });
+    // feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fdiem-den.rss")
+    // .subscribe(value => {
+    //   feedService.createListFeedItems(this.destinationFeed, value);
+    // });
+    // feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fgiai-tri.rss")
+    // .subscribe(value => {
+    //   feedService.createListFeedItems(this.relaxFeed, value);
+    // });
+    // feedService.getDataRss("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnld.com.vn%2Fgiao-duc.rss")
+    // .subscribe(value => {
+    //   // console.log(value.items[0].link);
+    //   this.link = value.items[0].link;
+    //   // console.log(this.link)
+    //   this.onSend(this.link);
+    //   // console.log('link: ' + this.link);
+    //   this.feedService.onSendService(this.formData).subscribe(
+    //     res =>{
+    //       this.cover = res.content[0]
+    //       // console.log('cover: ' + this.cover);
+    //     },
+    //     error => {
+    //       console.log(error);
+    //     }
+    //   )
+    //   feedService.createListFeedItems(this.eduFeed, value);
+    // });
 
    }
 
@@ -146,16 +145,89 @@ export class HomeComponent implements OnInit {
     //   );
     // }
 
-    this.formData.append('category', 'kinh tế');
+    this.formData.append('category', 'tin mới nhất');
     this.feedService.onSendLoadData(this.formData).subscribe(
       res => {
-        this.feedService.loadDataFeed(this.internalFeed, res);
-        console.log(this.internalFeed);
+        this.feedService.loadDataFeed(this.newsFeed, res);
+        console.log("tin moi nhat");
+        console.log(this.newsFeed[0].title);
+        // console.log(res);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+    this.formData.append('category', 'giáo dục');
+    this.feedService.onSendLoadData(this.formData).subscribe(
+      res => {
+        this.feedService.loadDataFeed(this.newsFeed, res);
+        console.log(this.eduFeed);
       },
       error => {
         // console.log(error);
       }
     );
+
+    this.formData.append('category', 'công đoàn');
+    this.feedService.onSendLoadData(this.formData).subscribe(
+      res => {
+        this.feedService.loadDataFeed(this.unionFeed, res);
+        console.log(this.eduFeed);
+      },
+      error => {
+        // console.log(error);
+      }
+    );
+    this.formData.append('category', 'giáo dục');
+    this.feedService.onSendLoadData(this.formData).subscribe(
+      res => {
+        this.feedService.loadDataFeed(this.eduFeed, res);
+        console.log('giáo dục: ')
+        console.log(res);
+      },
+      error => {
+        // console.log(error);
+      }
+    );
+
+    this.formData.append('category', 'công nghệ');
+    this.feedService.onSendLoadData(this.formData).subscribe(
+      res => {
+        this.feedService.loadDataFeed(this.techniqueFeed, res);
+      },
+      error => {
+        // console.log(error);
+      }
+    );
+    this.formData.append('category', 'địa ốc');
+    this.feedService.onSendLoadData(this.formData).subscribe(
+      res => {
+        this.feedService.loadDataFeed(this.estateFeed, res);
+      },
+      error => {
+        // console.log(error);
+      }
+    );
+
+    this.formData.append('category', 'giải trí');
+    this.feedService.onSendLoadData(this.formData).subscribe(
+      res => {
+        this.feedService.loadDataFeed(this.relaxFeed, res);
+      },
+      error => {
+        // console.log(error);
+      }
+    );
+    this.formData.append('category', 'điểm đến');
+    this.feedService.onSendLoadData(this.formData).subscribe(
+      res => {
+        this.feedService.loadDataFeed(this.destinationFeed, res);
+      },
+      error => {
+        // console.log(error);
+      }
+    );
+
 
 
   }
